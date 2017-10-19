09fd49099779a94264e324a798460fbb0ac996bf
baku171 <pbak@esri.pl>
2017-10-19 14:39:39 +0200

    Nazwy plików

--- modules/handlers.js
+++ modules/handlers.js
@@ -1,11 +1,19 @@
 var fs = require("fs");
 var formidable = require("formidable");
 
+var fileName;
+
+
 exports.upload = function(request, response) {
     console.log("Rozpoczynam obsługę żądania upload.");
     var form = new formidable.IncomingForm();
     form.parse(request, function(err, fields, files) {
-        fs.renameSync(files.upload.path, "test.png");
+        if (fields.title.trim()) {
+            fileName = (fields.title.trim() + ".png");
+        } else {
+            fileName = files.upload.name;
+        };        
+        fs.renameSync(files.upload.path, fileName);
         response.writeHead(200, {"Content-Type": "text/html"});
         response.write("received image:<br/>");
         response.write("<img src='/show' />");
@@ -29,10 +37,10 @@ exports.error = function(request, response) {
 }
 
 exports.show = function(request, response) {
-    console.log("Przekazuję obraz.");
-    fs.readFile("test.png","binary", function(err, image) {
+    console.log("Przekazuję obraz:" + fileName);
+    fs.readFile(fileName,"binary", function(err, image) {
         response.writeHead(200, {"Content-Type": "image-png"});
         response.write(image, "binary");
         response.end();
-    })
+    });
 }
